01ad133336d67ff82de65e4457c07a9dab1c340d
dimitrios <dimitrios@local.local>
2020-12-17 17:33:56 +0200

    Add sex enumeration.

---
+++ Shared/Enumerations/Sex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gizmo
+{
+    /// <summary>
+    /// Sex enumeration.
+    /// </summary>
+    public enum Sex
+    {
+        /// <summary>
+        /// Unspecified.
+        /// </summary>
+        Unspecified = 0,
+        /// <summary>
+        /// Male.
+        /// </summary>
+        Male = 1,
+        /// <summary>
+        /// Female.
+        /// </summary>
+        Female = 2,
+    }
+}
