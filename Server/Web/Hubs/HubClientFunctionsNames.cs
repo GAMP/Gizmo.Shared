5798503c654c76475635f5f6bf0ac32f8ddf63fb
Oleg Savelos <olegsavelos@gmail.com>
2021-01-23 18:46:53 +0200

    Some changes.

---
+++ Server/Web/Hubs/HubClientFunctionsNames.cs
@@ -0,0 +1,17 @@
+﻿namespace Gizmo.Server.Web.Hubs
+{
+    /// <summary>
+    /// Contains definitions for hub event functions.
+    /// </summary>
+    public static class HubClientFunctionsNames
+    {
+        #region CONSTANTS
+        
+        public const string ENTITY_EVENT_NAME = "EntityEvent";
+        public const string USER_EVENT_NAME = "UserEvent";
+        public const string PRODUCT_EVENT_NAME = "UserEvent";
+        public const string HOST_EVENT_NAME = "UserEvent"; 
+
+        #endregion
+    }
+}
