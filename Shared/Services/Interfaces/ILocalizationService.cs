964e82bf1ebcc9eb0d9c6c304498b4965bb80696
Oleg Savelos <olegsavelos@gmail.com>
2021-08-16 14:49:39 +0300

    Added localization service.

---
+++ Shared/Services/Interfaces/ILocalizationService.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Gizmo.Shared.Services
+{
+    public interface ILocalizationService
+    {
+        #region PROPERTIES
+        
+        /// <summary>
+        /// Gets a list of available cultures.
+        /// </summary>
+        public IEnumerable<RegionInfo> SupportedRegions { get; }
+
+        #endregion
+
+        #region FUNCTIONS
+
+        /// <summary>
+        /// Gets localized string value.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <returns>Localized string value.</returns>
+        string GetString(string key);
+
+        /// <summary>
+        /// Gets localized string value.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <param name="arguments">Arguments.</param>
+        /// <returns>Localized string value.</returns>
+        string GetString(string key, params object[] arguments);
+
+        /// <summary>
+        /// Gets localized string upper variant value.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <returns>Localized string value.</returns>
+        string GetStringUpper(string key);
+
+        /// <summary>
+        /// Gets localized string upper variant value.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <param name="arguments">Arguments.</param>
+        /// <returns>Localized string value.</returns>
+        string GetStringUpper(string key, params object[] arguments);
+
+        /// <summary>
+        /// Gets localized string lower variant value.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <returns>Localized string value.</returns>
+        string GetStringLower(string key);
+
+        /// <summary>
+        /// Gets localized string lower variant value.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <param name="arguments">Arguments.</param>
+        /// <returns>Localized string value.</returns>
+        string GetStringLower(string key, params object[] arguments);
+
+        /// <summary>
+        /// Sets current culture.
+        /// </summary>
+        /// <param name="culture">Desired culture.</param>
+        void SetCurrentCulture(CultureInfo culture); 
+
+        #endregion
+    }
+}
---
+++ Shared/Services/LocalizationServiceBase.cs
@@ -0,0 +1,123 @@
+﻿using Gizmo.Shared.Services;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Gizmo.Shared.Shared.Services
+{
+    /// <summary>
+    /// Localization service base.
+    /// </summary>
+    public abstract class LocalizationServiceBase : ILocalizationService
+    {
+        #region CONSTRUCTOR
+        public LocalizationServiceBase(ILogger logger, IStringLocalizer stringLocalizer)
+        {
+            _logger = logger;
+            _localizer = stringLocalizer;
+        }
+        #endregion
+
+        #region FIELDS
+
+        #region PRIVATE
+
+        private readonly object[] _DEFAULT_ARGS = System.Array.Empty<object>();
+        private readonly IStringLocalizer _localizer;
+        private readonly ILogger _logger;
+        private readonly HashSet<RegionInfo> _cultures = new()
+        {
+            new RegionInfo("us"),
+            new RegionInfo("ru"),
+            new RegionInfo("gr")
+        };
+
+        #endregion
+
+        #region PUBLIC
+
+        /// <inheritdoc/>
+        public IEnumerable<RegionInfo> SupportedRegions => _cultures;
+
+        #endregion
+
+        #endregion
+
+        #region PROPERTIES
+
+        #region PRIVATE
+
+        /// <summary>
+        /// Gets localizer instance.
+        /// </summary>
+        private IStringLocalizer Localizer
+        {
+            get { return _localizer; }
+        }
+
+        /// <summary>
+        /// Gets logger instance.
+        /// </summary>
+        private ILogger Logger
+        {
+            get { return _logger; }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region FUNCTIONS
+
+        #region PUBLIC
+
+        /// <inheritdoc/>
+        public string GetString(string key)
+        {
+            return GetString(key, _DEFAULT_ARGS);
+        }
+
+        /// <inheritdoc/>
+        public string GetString(string key, params object[] arguments)
+        {
+            return Localizer.GetString(key, arguments);
+        }
+
+        /// <inheritdoc/>
+        public string GetStringUpper(string key)
+        {
+            return GetStringUpper(key, _DEFAULT_ARGS);
+        }
+
+        /// <inheritdoc/>
+        public string GetStringUpper(string key, params object[] arguments)
+        {
+            return GetString(key, arguments)?.ToUpper();
+        }
+
+        /// <inheritdoc/>
+        public string GetStringLower(string key)
+        {
+            return GetStringLower(key, _DEFAULT_ARGS);
+        }
+
+        /// <inheritdoc/>
+        public string GetStringLower(string key, params object[] arguments)
+        {
+            return GetString(key, arguments)?.ToLower();
+        }
+
+        /// <inheritdoc/>
+        public void SetCurrentCulture(CultureInfo culture)
+        {
+            Logger.LogTrace($"Setting current culture to {culture}.");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
