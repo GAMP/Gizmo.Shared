d8ea7b92f2cf77de2e51675432dda67bcabafb0c
Oleg Savelos <olegsavelos@gmail.com>
2022-04-02 17:21:06 +0300

    Updates.

---
+++ Shared/Enumerations/FiscalReceiptPrintStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace Gizmo
+{
+    /// <summary>
+    /// Print status.
+    /// </summary>
+    /// <remarks>
+    /// This flag will be used to indicate receipt print status.
+    /// </remarks>
+    public enum FiscalReceiptPrintStatus
+    {
+        /// <summary>
+        /// No receipt required.
+        /// </summary>
+        None=0,
+        /// <summary>
+        /// Receipt printing is pending.
+        /// </summary>
+        Pending=1,
+        /// <summary>
+        /// Receipt was printed.
+        /// </summary>
+        Printed=2,
+        /// <summary>
+        /// Not all required receipts printed.
+        /// </summary>
+        PartialyPrinted=3,
+    }
+}
---
+++ Shared/Enumerations/FiscalReceiptType.cs
@@ -0,0 +1,29 @@
+﻿namespace Gizmo
+{
+    /// <summary>
+    /// Fiscal receipt type.
+    /// </summary>
+    public enum FiscalReceiptType 
+    {
+        /// <summary>
+        /// Invoice sale.
+        /// </summary>
+        Sale=0,
+        /// <summary>
+        /// Invoice void.
+        /// </summary>
+        VoidSale=1,
+        /// <summary>
+        /// Deposit sale.
+        /// </summary>
+        Deposit=2,
+        /// <summary>
+        /// Deposit void.
+        /// </summary>
+        VoidDeposit=3,
+        /// <summary>
+        /// Register transaction.
+        /// </summary>
+        RegisterTransaction=4,
+    }
+}
