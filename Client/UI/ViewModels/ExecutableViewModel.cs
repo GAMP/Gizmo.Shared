32d14ab5b37bf6a23b82584ee387f4caaf6b3d2f
klapsis-netprojects <klapsis@netprojects.gr>
2022-08-25 14:01:53 +0300

    Remove demo view models.

--- Client/UI/ViewModels/ExecutableViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gizmo.Shared.Client.UI.ViewModels
-{
-    public class ExecutableViewModel
-    {
-        public int Id { get; set; }
-
-        public string Caption { get; set; }
-    }
-}
