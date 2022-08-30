b110d0fd38bc57a66ae2e58334974d7dd08c2581
Oleg Savelos <olegsavelos@gmail.com>
2022-08-30 16:26:50 +0300

    Added name attribute.

---
+++ Shared/Attributes/NameAttribute.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+namespace System.ComponentModel.DataAnnotations
+{
+    /// <summary>
+    /// Name attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class| AttributeTargets.Property,AllowMultiple =false)]
+    public class NameAttribute : Attribute
+    {
+        #region CONSTRUCTOR
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        /// <exception cref="ArgumentNullException">thrown in case <paramref name="name"/>is equal to null or empty string.</exception>
+        public NameAttribute(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+            Name = name;
+        }
+        #endregion
+
+        #region PROPERTIES
+        /// <summary>
+        /// Gets name.
+        /// </summary>
+        public string Name
+        {
+            get;
+        } 
+        #endregion
+    }
+}
