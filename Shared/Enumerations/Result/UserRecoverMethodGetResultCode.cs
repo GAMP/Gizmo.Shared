74ecbf6ab05a6a6d35c1ae5eb13a185231622099
Oleg Savelos <olegsavelos@gmail.com>
2022-10-06 16:26:46 +0300

    Updates.

--- Shared/Enumerations/Result/UserRecoverMethodGetResultCode.cs
+++ Shared/Enumerations/Result/UserRecoverMethodGetResultCode.cs
@@ -1,18 +1,24 @@
 ﻿namespace Gizmo
 {
+    /// <summary>
+    /// User recovery method result codes.
+    /// </summary>
     public enum UserRecoverMethodGetResultCode
     {
         /// <summary>
         /// Success.
         /// </summary>
-        Success=0,
+        [Localized("USER_RECOVERY_METHOD_GET_RESULT_SUCCESS")]
+        Success = 0,
         /// <summary>
         /// Internal error.
         /// </summary>
-        Error=1,
+        [Localized("USER_RECOVERY_METHOD_GET_RESULT_ERROR")]
+        Error = 1,
         /// <summary>
         /// User not found.
         /// </summary>
-        UserNotFound=2,
+        [Localized("USER_RECOVERY_METHOD_GET_RESULT_USER_NOT_FOUND")]
+        UserNotFound = 2,
     }
 }
