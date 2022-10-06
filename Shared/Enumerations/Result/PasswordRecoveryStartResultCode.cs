ff50637b590aa2a9a9fb51eb8562af20e6ac9179
Oleg Savelos <olegsavelos@gmail.com>
2022-10-06 17:42:06 +0300

    Removed localization.

--- Shared/Enumerations/Result/PasswordRecoveryStartResultCode.cs
+++ Shared/Enumerations/Result/PasswordRecoveryStartResultCode.cs
@@ -10,32 +10,26 @@ namespace Gizmo
         /// <summary>
         /// Success.
         /// </summary>
-        [Localized("PASSWORD_RECOVERY_RESULT_CODE_SUCCESS")]
         Success = BASE_CODES.SUCCESS,
         /// <summary>
         /// Failed.
         /// </summary>
-        [Localized("PASSWORD_RECOVERY_RESULT_CODE_FAILURE")]
         Failed = BASE_CODES.FAILURE,
         /// <summary>
         /// No route.
         /// </summary>
-        [Localized("PASSWORD_RECOVERY_RESULT_NO_ROUTE")]
         NoRouteForDelivery = DELIVERY_ERROR_CODES.NO_ROUTE,
         /// <summary>
         /// Delivery failed.
         /// </summary>
-        [Localized("PASSWORD_RECOVERY_RESULT_CODE_DELIVERY_FAILED")]
         DeliveryFailed = DELIVERY_ERROR_CODES.DELIVERY_FAILED,
         /// <summary>
         /// Invalid input.
         /// </summary>
-        [Localized("PASSWORD_RECOVERY_RESULT_CODE_INVALID_INPUT")]
         InvalidInput = EXTENDED_ERROR_CODES.INVALID_INPUT,
         /// <summary>
         /// User not found.
         /// </summary>
-        [Localized("PASSWORD_RECOVERY_RESULT_CODE_USER_NOT_FOUND")]
         UserNotFound = EXTENDED_ERROR_CODES.USER_NOT_FOUND,
     }
 }
--- Shared/Enumerations/Result/UserRecoverMethodGetResultCode.cs
+++ Shared/Enumerations/Result/UserRecoverMethodGetResultCode.cs
@@ -8,17 +8,14 @@
         /// <summary>
         /// Success.
         /// </summary>
-        [Localized("USER_RECOVERY_METHOD_GET_RESULT_SUCCESS")]
         Success = 0,
         /// <summary>
         /// Internal error.
         /// </summary>
-        [Localized("USER_RECOVERY_METHOD_GET_RESULT_ERROR")]
         Error = 1,
         /// <summary>
         /// User not found.
         /// </summary>
-        [Localized("USER_RECOVERY_METHOD_GET_RESULT_USER_NOT_FOUND")]
         UserNotFound = 2,
     }
 }
