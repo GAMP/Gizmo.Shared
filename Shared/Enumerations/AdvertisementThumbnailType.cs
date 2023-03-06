2b8c2f978c85fb5a30fede322e2105aec463a754
Andrey Pestunov <andreypestunov@gmail.com>
2023-03-06 02:11:35 +0100

    feat: add news states

---
+++ Shared/Enumerations/AdvertisementThumbnailType.cs
@@ -0,0 +1,11 @@
+﻿namespace Gizmo
+{
+    /// <summary>
+    /// Advertisement Thumbnail Url type.
+    /// </summary>
+    public enum AdvertisementThumbnailType
+    {
+        Internal = 1 ,
+        External
+    }
+}
