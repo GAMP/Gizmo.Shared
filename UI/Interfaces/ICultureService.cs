96554eab72a023ef41259b6584218e5710d02b02
Oleg Savelos <olegsavelos@gmail.com>
2023-03-15 16:55:57 +0200

    Moved interface.

---
+++ UI/Interfaces/ICultureService.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Gizmo.UI
+{
+    /// <summary>
+    /// Culture service.
+    /// </summary>
+    /// <remarks>
+    /// Responsible of setting culture based on platform.
+    /// </remarks>
+    public interface ICultureService
+    {
+        /// <summary>
+        /// Sets current UI culture.
+        /// </summary>
+        /// <param name="culture">Culture.</param>
+        Task SetCurrentUICultureAsync(CultureInfo culture);
+    }
+}
