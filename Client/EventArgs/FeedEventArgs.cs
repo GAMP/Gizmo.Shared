b095c5e186809e4bf20bfa3d7a459bf9486e7710
Andrey Pestunov <masterlifting@gmail.com>
2023-03-22 16:55:44 +0100

    refactor: add client event args

---
+++ Client/EventArgs/FeedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace Gizmo.Client;
+
+/// <summary>
+/// Server data change args.
+/// </summary>
+public sealed class FeedEventArgs : ModificationEventArgs
+{
+    public FeedEventArgs(int entityId, ModificationType modificationType) : base(entityId, modificationType)
+    {
+    }
+}
---
+++ Client/EventArgs/ModificationEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Gizmo.Client;
+
+/// <summary>
+/// Modification Event Args.
+/// </summary>
+public abstract class ModificationEventArgs : EventArgs
+{
+    protected ModificationEventArgs(int entityId, ModificationType modificationType)
+    {
+        EntityId = entityId;
+        ModificationType = modificationType;
+    }
+
+    public int EntityId { get; init; }
+    public ModificationType ModificationType { get; init; }
+}
