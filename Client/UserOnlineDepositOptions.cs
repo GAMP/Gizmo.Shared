7e7cd7dada14cca7b6012994335bfd063400dbc7
klapsis-netprojects <klapsis@netprojects.gr>
2023-04-07 13:08:06 +0300

    Online deposit updates.

---
+++ Client/UserOnlineDepositOptions.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace Gizmo.Client.UI;
+
+/// <summary>
+/// Online deposit options.
+/// </summary>
+public sealed class UserOnlineDepositOptions
+{
+    public bool ShowUserOnlineDeposit { get; set; }
+    public decimal MaximumAmount { get; set; }
+}
