02851f6f8c05a72a8864ea8ae48ed9a65a0ce4a1
klapsis-netprojects <klapsis@netprojects.gr>
2023-04-12 15:50:36 +0300

    Add popular items options.

---
+++ Client/PopularItemsOptions.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace Gizmo.Client.UI
+{
+    /// <summary>
+    /// Popular items options.
+    /// </summary>
+    public sealed class PopularItemsOptions
+    {
+        public int PopularApplications { get; set; }
+        public int PopularExecutables { get; set; }
+        public int PopularProducts { get; set; }
+    }
+}
