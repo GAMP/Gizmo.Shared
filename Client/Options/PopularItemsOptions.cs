351f7a3bf34696363bd081b3db656f48add6dbed
klapsis-netprojects <klapsis@netprojects.gr>
2023-05-18 16:23:54 +0300

    Updates.

--- Client/Options/PopularItemsOptions.cs
+++ Client/Options/PopularItemsOptions.cs
@@ -7,8 +7,13 @@ namespace Gizmo.Client.UI
     /// </summary>
     public sealed class PopularItemsOptions
     {
-        public int PopularApplications { get; set; }
-        public int PopularExecutables { get; set; }
-        public int PopularProducts { get; set; }
+        public int MaxPopularProducts { get; set; }
+        public int MaxPopularApplications { get; set; }
+
+        public int MaxQuickLaunchExecutables { get; set; }
+
+        public int HomePageMaxItemsPerRow { get; set; }
+        public int AppsPageMaxItemsPerRow { get; set; }
+        public int ProductsPageMaxItemsPerRow { get; set; }
     }
 }
