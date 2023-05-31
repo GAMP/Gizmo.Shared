abee0d234d90783d3b6bdda0070c349fa6ae3e34
Oleg Savelos <olegsavelos@gmail.com>
2023-05-31 17:36:58 +0300

    Notification host.

---
+++ UI/Interfaces/INotificationsHostWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Gizmo.UI
+{
+    /// <summary>
+    /// Desktop UI notifications host window interface.
+    /// </summary>
+    public interface INotificationsHostWindow
+    {
+        /// <summary>
+        /// Shows host window.
+        /// </summary>
+        public Task ShowAsync();
+
+        /// <summary>
+        /// Hides host window.
+        /// </summary>
+        public Task HideAsyc();
+    }
+}
