e9bad3933b79e51248446d4fc7cb6d3a8e63ae0e
klapsis-netprojects <klapsis@netprojects.gr>
2023-06-13 13:03:05 +0300

    Update resources.

--- Shared/Enumerations/ApplicationModes.cs
+++ Shared/Enumerations/ApplicationModes.cs
@@ -15,72 +15,72 @@ namespace Gizmo
         /// <summary>
         /// Single player.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_SINGLE_PLAYER")]
+        [Localized("GIZ_EXECUTABLE_MODE_SINGLE_PLAYER")]
         SinglePlayer = 1,
         /// <summary>
         /// Online multiplayer.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_ONLINE")]
+        [Localized("GIZ_EXECUTABLE_MODE_ONLINE")]
         Online = 2,
         /// <summary>
         /// Lan Multiplayer.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_MULTIPLAYER")]
+        [Localized("GIZ_EXECUTABLE_MODE_MULTIPLAYER")]
         Multiplayer = 4,
         /// <summary>
         /// Settings.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_SETTINGS")]
+        [Localized("GIZ_EXECUTABLE_MODE_SETTINGS")]
         Settings = 8,
         /// <summary>
         /// Utility.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_UTILITY")]
+        [Localized("GIZ_EXECUTABLE_MODE_UTILITY")]
         Utility = 16,
         /// <summary>
         /// Game.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_GAME")]
+        [Localized("GIZ_EXECUTABLE_MODE_GAME")]
         Game = 32,
         /// <summary>
         /// Application.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_APPLICATION")]
+        [Localized("GIZ_EXECUTABLE_MODE_APPLICATION")]
         Application = 64,
         /// <summary>
         /// Free to play.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_FREE_TO_PLAY")]
+        [Localized("GIZ_EXECUTABLE_MODE_FREE_TO_PLAY")]
         FreeToPlay = 128,
         /// <summary>
         /// Requires subscription.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_REQUIRES_SUBSCRIPTION")]
+        [Localized("GIZ_EXECUTABLE_MODE_REQUIRES_SUBSCRIPTION")]
         RequiresSubscription = 256,
         /// <summary>
         /// Free trial.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_FREE_TRIAL")]
+        [Localized("GIZ_EXECUTABLE_MODE_FREE_TRIAL")]
         FreeTrial = 512,
         /// <summary>
         /// Split screen.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_SPLIT_SCREEN_MULTIPLAYER")]
+        [Localized("GIZ_EXECUTABLE_MODE_SPLIT_SCREEN_MULTIPLAYER")]
         SplitScreenMultiPlayer = 1024,
         /// <summary>
         /// Lan co-op.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_CO_OP")]
+        [Localized("GIZ_EXECUTABLE_MODE_CO_OP")]
         CoOpLan = 2048,
         /// <summary>
         /// Online co-op.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_CO_OP_ONLINE")]
+        [Localized("GIZ_EXECUTABLE_MODE_CO_OP_ONLINE")]
         CoOpOnline = 4096,
         /// <summary>
         /// One time purchase.
         /// </summary>
-        [Localized("EXECUTABLE_MODE_ONE_TIME_PURCHASE")]
+        [Localized("GIZ_EXECUTABLE_MODE_ONE_TIME_PURCHASE")]
         OneTimePurchase = 8192,
     }
 }
