20e9fe725bfa6558d01df87b0d661847567b1787
Oleg Savelos <olegsavelos@gmail.com>
2023-06-21 08:44:03 +0300

    ClientOptions.

--- Client/Options/ClientOptions.cs
+++ Client/Options/ClientOptions.cs
@@ -3,60 +3,63 @@ using Gizmo.Client.UI;
 using Gizmo.UI;
 using MessagePack;
 
-[MessagePackObject(false)]
-public sealed class ClientOptions
+namespace Gizmo.Client
 {
-    [Key(0)]
-    [JsonPropertyOrder(0)]
-    [JsonPropertyName("Interface")]
-    public ClientUIOptions UserInterface { get; init; } = new ClientUIOptions();
+    [MessagePackObject(false)]
+    public sealed class ClientOptions
+    {
+        [Key(0)]
+        [JsonPropertyOrder(0)]
+        [JsonPropertyName("Interface")]
+        public ClientUIOptions UserInterface { get; init; } = new ClientUIOptions();
 
 
-    [Key(1)]
-    [JsonPropertyOrder(1)]
-    [JsonPropertyName("CurrencyOptions")]
-    public CurrencyOptions Currency { get; init; } = new CurrencyOptions();
+        [Key(1)]
+        [JsonPropertyOrder(1)]
+        [JsonPropertyName("CurrencyOptions")]
+        public CurrencyOptions Currency { get; init; } = new CurrencyOptions();
 
 
-    [Key(2)]
-    [JsonPropertyOrder(2)]
-    [JsonPropertyName("ClientReservationOptions")]
-    public ClientReservationOptions Reservations { get; init; } = new ClientReservationOptions();
+        [Key(2)]
+        [JsonPropertyOrder(2)]
+        [JsonPropertyName("ClientReservationOptions")]
+        public ClientReservationOptions Reservations { get; init; } = new ClientReservationOptions();
 
 
-    [Key(3)]
-    [JsonPropertyOrder(3)]
-    [JsonPropertyName("FeedsOptions")]
-    public FeedsOptions Feeds { get; init; } = new FeedsOptions();
+        [Key(3)]
+        [JsonPropertyOrder(3)]
+        [JsonPropertyName("FeedsOptions")]
+        public FeedsOptions Feeds { get; init; } = new FeedsOptions();
 
 
-    [Key(4)]
-    [JsonPropertyOrder(4)]
-    [JsonPropertyName("HostQRCodeOptions")]
-    public HostQRCodeOptions HostQRCode { get; init; } = new HostQRCodeOptions();
+        [Key(4)]
+        [JsonPropertyOrder(4)]
+        [JsonPropertyName("HostQRCodeOptions")]
+        public HostQRCodeOptions HostQRCode { get; init; } = new HostQRCodeOptions();
 
 
-    [Key(5)]
-    [JsonPropertyOrder(5)]
-    [JsonPropertyName("LoginRotatorOptions")]
-    public LoginRotatorOptions LoginRotator { get; init; } = new LoginRotatorOptions();
+        [Key(5)]
+        [JsonPropertyOrder(5)]
+        [JsonPropertyName("LoginRotatorOptions")]
+        public LoginRotatorOptions LoginRotator { get; init; } = new LoginRotatorOptions();
 
 
-    [Key(6)]
-    [JsonPropertyOrder(6)]
-    [JsonPropertyName("PopularItemsOptions")]
-    public PopularItemsOptions PopularItems { get; init; } = new PopularItemsOptions();
+        [Key(6)]
+        [JsonPropertyOrder(6)]
+        [JsonPropertyName("PopularItemsOptions")]
+        public PopularItemsOptions PopularItems { get; init; } = new PopularItemsOptions();
 
 
-    [Key(7)]
-    [JsonPropertyOrder(7)]
-    [JsonPropertyName("UserLoginOptions")]
-    public UserLoginOptions UserLogin { get; init; } = new UserLoginOptions();
+        [Key(7)]
+        [JsonPropertyOrder(7)]
+        [JsonPropertyName("UserLoginOptions")]
+        public UserLoginOptions UserLogin { get; init; } = new UserLoginOptions();
 
 
-    [Key(8)]
-    [JsonPropertyOrder(8)]
-    [JsonPropertyName("UserOnlineDepositOptions")]
-    public UserOnlineDepositOptions UserOnlineDeposit { get; init; } = new UserOnlineDepositOptions();
+        [Key(8)]
+        [JsonPropertyOrder(8)]
+        [JsonPropertyName("UserOnlineDepositOptions")]
+        public UserOnlineDepositOptions UserOnlineDeposit { get; init; } = new UserOnlineDepositOptions();
 
+    }
 }
