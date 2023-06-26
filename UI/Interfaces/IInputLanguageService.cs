38f188c4a0b09017e5513dbdc9ef6bebd9afa559
Oleg Savelos <olegsavelos@gmail.com>
2023-06-26 15:43:32 +0300

    fix language change.

--- UI/Interfaces/IInputLanguageService.cs
+++ UI/Interfaces/IInputLanguageService.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Threading.Tasks;
@@ -9,10 +10,19 @@ namespace Gizmo.UI
     public interface IInputLanguageService
     {
         /// <summary>
+        /// Occures once current language changes.
+        /// </summary>
+        event EventHandler<EventArgs> LangauageChange;
+
+        /// <summary>
         /// Gets available input languages.
         /// </summary>
         IEnumerable<CultureInfo> AvailableInputLanguages { get; }
+
         CultureInfo? GetLanguage(string twoLetterISOLanguageName);
+        
         Task SetCurrentInputLanguageAsync(CultureInfo currentInputLanguage);
+        
+        CultureInfo CurrentInputLanguage { get; }
     }
 }
