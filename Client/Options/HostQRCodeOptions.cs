ad31fe5abf3ca4aa0ec05c58f6907cd2d00cb7da
klapsis-netprojects <klapsis@netprojects.gr>
2023-08-18 09:51:34 +0300

    Task 375.

--- Client/Options/HostQRCodeOptions.cs
+++ Client/Options/HostQRCodeOptions.cs
@@ -14,5 +14,15 @@ namespace Gizmo.Client.UI
         [MessagePack.Key(0)]
         [DefaultValue(false)]
         public bool Enabled { get; set; } = false;
+
+        /// <summary>
+        /// Indicates if host qr code is base 64.
+        /// </summary>
+        /// <remarks>
+        /// False by default.
+        /// </remarks>
+        [MessagePack.Key(1)]
+        [DefaultValue(false)]
+        public bool IsBase64 { get; set; } = false;
     }
 }
