5a7be39eb21724345cee28225faa1be25d836b4b
Oleg Savelos <olegsavelos@gmail.com>
2024-01-31 18:16:11 +0200

    Suggestions support.

---
+++ UI/View/Services/ISuggestionService.cs
@@ -0,0 +1,32 @@
+﻿using Gizmo.UI.View.States;
+using System.Threading.Tasks;
+
+namespace Gizmo.UI.View.Services
+{
+    /// <summary>
+    /// Suggestion service.
+    /// </summary>
+    /// <remarks>
+    /// Implemented by services providing suggestions for selection.
+    /// </remarks>
+    public interface ISuggestionService : IViewService
+    {
+        /// <summary>
+        /// Sets current suggestion pattern.
+        /// </summary>
+        /// <param name="pattern">Pattern.</param>
+        public Task SetSuggestionPatternAsync(string pattern);
+
+        /// <summary>
+        /// Gets suggestion view state.
+        /// </summary>
+        /// <param name="value">Suggestion value.</param>
+        /// <returns></returns>
+        public ISuggestionViewState GetSuggestionViewState(object value);
+
+        /// <summary>
+        /// Gets suggestions view state.
+        /// </summary>
+        public ISuggestionsViewState ViewState { get; }
+    }
+}
---
+++ UI/View/States/ISuggestionsViewState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Gizmo.UI.View.States
+{
+    /// <summary>
+    /// Generic suggestions view state interface.
+    /// </summary>
+    public interface ISuggestionsViewState : IValidatingViewState
+    {
+        /// <summary>
+        /// Gets current pattern.
+        /// </summary>
+        public string? Pattern { get; set; }
+
+        /// <summary>
+        /// Gets current suggestions results.
+        /// </summary>
+        IEnumerable<ISuggestionViewState> Suggestions { get; }
+    }
+
+
+    /// <summary>
+    /// Typed suggestions view state interface.
+    /// </summary>
+    /// <typeparam name="TSuggestion">Suggestion view state type.</typeparam>
+    public interface ISuggestionsViewState<TSuggestion> : ISuggestionsViewState where TSuggestion : ISuggestionViewState
+    {
+        /// <summary>
+        /// Gets suggestions.
+        /// </summary>
+        new IEnumerable<TSuggestion> Suggestions { get; }
+    }
+}
