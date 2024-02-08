9cc8401baca6b2a86ee4b49c8a5c39186ce47b56
Oleg Savelos <olegsavelos@gmail.com>
2024-02-08 09:41:20 +0200

    serialization updates.

--- Shared/Microsoft/Extensions/Options/Models/StoreOptionsReadPack.cs
+++ Shared/Microsoft/Extensions/Options/Models/StoreOptionsReadPack.cs
@@ -1,4 +1,5 @@
 ﻿#nullable enable
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -17,12 +18,32 @@ namespace Microsoft.Extensions.Options
         /// Creates new instance.
         /// </summary>
         /// <param name="optionsType">Options type.</param>
+        /// <param name="groupName">Group name.</param>
+        /// <param name="section">Section.</param>
+        /// <param name="values">Options values.</param>
+        [JsonConstructor()]
+        public StoreOptionsReadPack(string optionsType, string groupName, string? section, IEnumerable<StoreOptionsPair> values)
+        {
+            OptionsType = optionsType;
+            GroupName = groupName;
+            Section = section;
+            Values = values;
+            ValueStore = values.ToDictionary(x => x.Metadata, x => x.Value);
+        }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="optionsType">Options type.</param>
+        /// <param name="groupName">Group name.</param>
+        /// <param name="section">Section.</param>
         /// <param name="values">Options values.</param>
         public StoreOptionsReadPack(string optionsType, string groupName, string? section, Dictionary<StoreOptionMetadata, StoreOptionReadValue> values)
         {
             OptionsType = optionsType;
             GroupName = groupName;
             Section = section;
+            Values = values.Select(x=> new  StoreOptionsPair(x.Key,x.Value));
             ValueStore = values;
         }
 
@@ -54,25 +75,56 @@ namespace Microsoft.Extensions.Options
         [MessagePack.Key(2)]
         public string? Section
         {
-            get; init;
+            get; 
         }
 
         /// <summary>
         /// Gets options metadata and current values.
         /// </summary>
         [MessagePack.IgnoreMember()]
-        public IEnumerable<StoreOptionMetadataValue> Values
+        public IEnumerable<StoreOptionsPair> Values
         {
-            get { return ValueStore.Select(kv => new StoreOptionMetadataValue(kv.Key, kv.Value)); }
-        }
+            get;
+        } = [];
 
         /// <summary>
         /// Gets options metadata and current values.
         /// </summary>
         [JsonIgnore()]
         [MessagePack.Key(3)]
-        public Dictionary<StoreOptionMetadata, StoreOptionReadValue> ValueStore { get; }
+        public Dictionary<StoreOptionMetadata, StoreOptionReadValue> ValueStore { get; } = [];
 
         #endregion
+    }
+
+    /// <summary>
+    /// Store options pair.
+    /// </summary>
+    [MessagePack.MessagePackObject()]
+    public class StoreOptionsPair
+    {
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="metadata"></param>
+        /// <param name="value"></param>
+        [JsonConstructor()]       
+        public StoreOptionsPair(StoreOptionMetadata metadata, StoreOptionReadValue value)
+        {
+            Metadata = metadata;
+            Value = value;
+        }
+
+        /// <summary>
+        /// Option metadata.
+        /// </summary>
+        [MessagePack.Key(0)]
+        public StoreOptionMetadata Metadata { get; }
+
+        /// <summary>
+        /// Option value.
+        /// </summary>
+        [MessagePack.Key(1)]
+        public StoreOptionReadValue Value { get; }
     }
 }
