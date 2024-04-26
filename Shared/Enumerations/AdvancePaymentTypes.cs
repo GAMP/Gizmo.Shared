2c388f51bebdc406770f1247393b369956b24bf7
Oleg Savelos <olegsavelos@gmail.com>
2024-04-26 14:36:08 +0300

    new enums.

---
+++ Shared/Enumerations/AdvancePaymentTypes.cs
@@ -0,0 +1,32 @@
+﻿namespace Gizmo
+{
+    /// <summary>
+    /// Advance payment types.
+    /// </summary>
+    /// <remarks>
+    /// In payment system this describes the prepayment/advance payments and similar.
+    /// </remarks>
+    public enum AdvancePaymentTypes
+    {
+        /// <summary>
+        /// Full prepayment (full_prepayment).
+        /// </summary>
+        [Localized("ADVANCE_PAYMENTS_RU_FULL_PREPAYMENT")]
+        RU_full_prepayment = 0,
+        /// <summary>
+        /// Prepayment (prepayment).
+        /// </summary>
+        [Localized("ADVANCE_PAYMENTS_RU_PREPAYMENT")]
+        RU_prepayment = 1,
+        /// <summary>
+        /// Advance (advance).
+        /// </summary>
+        [Localized("ADVANCE_PAYMENTS_RU_ADVANCE")]
+        RU_advance = 2,
+        /// <summary>
+        /// Full payment (full_payment).
+        /// </summary>
+        [Localized("ADVANCE_PAYMENTS_RU_FULL_PAYMENT")] 
+        RU_full_payment = 3,
+    }
+}
---
+++ Shared/Enumerations/VatRates.cs
@@ -0,0 +1,29 @@
+﻿namespace Gizmo
+{
+    /// <summary>
+    /// Global VAT rates enumeration.
+    /// </summary>
+    public enum VatRates
+    {
+        /// <summary>
+        /// None (none)
+        /// </summary>
+        [Localized("VAT_RATES_RU_NONE")]
+        RU_none = 0,
+        /// <summary>
+        /// Zero VAT (vat0)
+        /// </summary>
+        [Localized("VAT_RATES_RU_VAT0")]
+        RU_vat0 = 1,
+        /// <summary>
+        /// 10% VAT (vat10)
+        /// </summary>
+        [Localized("VAT_RATES_RU_VAT10")]
+        RU_vat10 = 2,
+        /// <summary>   
+        /// 20% VAT (vat20)
+        /// </summary>
+        [Localized("VAT_RATES_RU_VAT20")]
+        RU_vat20 = 3,
+    }
+}
