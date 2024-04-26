fe0ec66a67528d27c6fa4877260f62a14a810436
Oleg Savelos <olegsavelos@gmail.com>
2024-04-26 17:39:08 +0300

    add tax systems.

---
+++ Shared/Enumerations/TaxSystems.cs
@@ -0,0 +1,14 @@
+﻿namespace Gizmo
+{
+    /// <summary>
+    /// Tax systems.
+    /// </summary>
+    public enum TaxSystems
+    {
+        RU_Main = 0,
+        RU_SimplifiedSystemIncomeTaxation = 1,
+        RU_SimplifiedSystemIncomeMinusExpenses = 2,
+        RU_SingleAgriculturalTax = 3,
+        RU_PatentSystem = 4    
+    }
+}
