14926b06fb8281a4e6eeb4e3cfe53bd1ac3196a6
Oleg Savelos <olegsavelos@gmail.com>
2024-11-26 17:36:38 +0200

    serialization fix.

--- Server/Options/GeneralOptions.cs
+++ Server/Options/GeneralOptions.cs
@@ -11,12 +11,14 @@ namespace Gizmo.Server.Options
     /// </summary>
     [OptionsConfigurationSection("GENERAL")]
     [StoreOptionsGroup("GENERAL")]
+    [MessagePack.MessagePackObject()]
     public sealed class GeneralOptions : IStoreOptions
     {
         [Name("Default culture", "SERVER_OPTION_DEFAULT_CULTURE_NAME")]
         [ExtendedDescription("Specifies default culture", "SERVER_OPTION_DEFAULT_CULTURE_DESCRIPTION")]
         [StoreOptionKey("DEFAULT_CULTURE")]
         [DefaultValue("en-US")]
+        [MessagePack.Key(0)]
         public string? DefaultCulture
         {
             get; init;
