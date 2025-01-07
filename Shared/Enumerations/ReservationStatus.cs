21ac73cac685043da85616a7bf6aa2ef0c11cc45
Oleg Savelos <olegsavelos@gmail.com>
2025-01-07 16:03:40 +0200

    updates.

--- Shared/Enumerations/ReservationStatus.cs
+++ Shared/Enumerations/ReservationStatus.cs
@@ -11,8 +11,23 @@
         Waiting = 0,
 
         /// <summary>
-        /// Reservation is canceled by operator.
+        /// Reservation is canceled by user or operator.
         /// </summary>
         Canceled = 1,
+
+        /// <summary>
+        /// Reservation is active.
+        /// </summary>
+        Active = 2,
+
+        /// <summary>
+        /// Reservation is expired.
+        /// </summary>
+        Expired = 3,
+
+        /// <summary>
+        /// Reservation is completed.
+        /// </summary>
+        Completed = 4,
     }
 }
