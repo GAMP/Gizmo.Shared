04f42b6e5a6b1c15d3b959022ce5de2b5f10824b
Oleg Savelos <olegsavelos@gmail.com>
2025-01-20 15:39:46 +0200

    updates.

--- Server/Options/HttpsCertificateOptions.cs
+++ Server/Options/HttpsCertificateOptions.cs
@@ -13,17 +13,20 @@ namespace Gizmo.Server.Options
     {
         [Name("HTTPS certificate type", "SERVER_OPTION_HTTPS_CERTIFICATE_TYPE_NAME")]
         [DefaultValue(null)]
-        [StoreOptionKey("TYPE")]     
+        [StoreOptionKey("TYPE")]
+        [MessagePack.Key(0)]
         public HttpsCertificateType? Type { get; init; }
 
         [Name("HTTPS certificate", "SERVER_OPTION_HTTPS_CERTIFICATE_NAME")]
         [DefaultValue(null)]
         [StoreOptionKey("CERTIFICATE")]
+        [MessagePack.Key(1)]
         public string? Certificate {  get; init; }
 
         [Name("HTTPS certificate password", "SERVER_OPTION_HTTPS_CERTIFICATE_PASSWORD_NAME")]
         [DefaultValue(null)]
         [StoreOptionKey("CERTIFICATE_PASSWORD")]
+        [MessagePack.Key(1)]
         public string? Password { get; init; }
     }
 }
