f5b364ff32a64c078d8ea4ebcb26b870c80d3936
klapsis-netprojects <klapsis@netprojects.gr>
2025-03-21 14:54:56 +0200

    Add skin options.

---
+++ Server/Options/SkinOptions.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+
+using Microsoft.Extensions.Options;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Server.Options
+{
+    /// <summary>
+    /// Skin options.
+    /// </summary>
+    [OptionsConfigurationSection("SKIN")]
+    [StoreOptionsGroup("SKIN")]
+    [MessagePack.MessagePackObject()]
+    public sealed class SkinOptions : IStoreOptions
+    {
+        /// <summary>
+        /// Gets default skin.
+        /// </summary>
+        [Name("Default skin", "SERVER_OPTION_DEFAULT_SKIN")]
+        [ExtendedDescription("Specifies default skin", "SERVER_OPTION_DEFAULT_SKIN")]
+        [StoreOptionKey("DEFAULT_SKIN")]
+        [DefaultValue(null)]
+        [MessagePack.Key(0)]
+        public string? DefaultSkin
+        {
+            get; set;
+        }
+    }
+}
