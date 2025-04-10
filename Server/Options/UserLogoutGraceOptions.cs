cd71950e169f8f70fd8558ea81200d64351c623d
Oleg Savelos <olegsavelos@gmail.com>
2025-04-10 17:16:45 +0300

    updates.

---
+++ Server/Options/UserLogoutGraceOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Server.Options
+{
+    [OptionsConfigurationSection("USERLOGOUTGRACE")]
+    [StoreOptionsGroup("USER_LOGOUT_GRACE")]
+    [MessagePack.MessagePackObject()]
+    public sealed class UserLogoutGraceOptions : IStoreOptions
+    {
+        [Name("Logout enabled")]
+        [ExtendedDescription("Specifies if age is mandatory for all age restrictions")]
+        [StoreOptionKey("ENABLED")]
+        [MessagePack.Key(0)]
+        public bool IsEnabled { get; init; }
+
+        [Name("Logout time")]
+        [ExtendedDescription("Specifies logout grace period time")]
+        [StoreOptionKey("TIME")]
+        [MessagePack.Key(1)]
+        public int Time { get; init; }
+    }
+}
