c013930da4da4319ff33e6e416be7cf8b3ee97b4
Oleg Savelos <olegsavelos@gmail.com>
2025-04-14 10:42:54 +0300

    deployment options.

---
+++ Server/Options/DeploymentOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Server.Options
+{
+    [OptionsConfigurationSection("DEPLOYMENT")]
+    [StoreOptionsGroup("DEPLOYMENT")]
+    [MessagePack.MessagePackObject()]
+    public sealed class DeploymentOptions : IStoreOptions
+    {
+        [Name("Disk space allocation")]
+        [ExtendedDescription("Specifies disk space allocation")]
+        [DefaultValue(DiskSpaceAllocations.Zero)]
+        [StoreOptionKey("DISK_SPACE_ALLOCATION")]
+        [MessagePack.Key(0)]
+        public DiskSpaceAllocations DiskSpaceAllocation
+        {
+            get;init;
+        }
+    }
+}
