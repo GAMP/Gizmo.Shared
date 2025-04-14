78c21c6cc99a54f70ea905e4925e6023184445e4
Oleg Savelos <olegsavelos@gmail.com>
2025-04-14 10:43:10 +0300

    deployment options.

---
+++ Shared/Enumerations/DiskSpaceAllocations.cs
@@ -0,0 +1,34 @@
+﻿namespace Gizmo
+{ 
+    public enum DiskSpaceAllocations
+    {
+        /// <summary>
+        /// Zero.
+        /// </summary>
+        Zero = 0,
+        /// <summary>
+        /// Five percent.
+        /// </summary>
+        Five = 5,
+        /// <summary>
+        /// Ten percent.
+        /// </summary>
+        Ten = 10,
+        /// <summary>
+        /// Fifteen percent.
+        /// </summary>
+        FifTeen = 15,
+        /// <summary>
+        /// Twenty percent.
+        /// </summary>
+        Twenty = 20,
+        /// <summary>
+        /// Twenty five percent.
+        /// </summary>
+        TwentyFive = 25,
+        /// <summary>
+        /// Thirty percent.
+        /// </summary>
+        Thirty = 30,
+    }
+}
