94dc8b8fba5ed5f444274517009831c784074ff8
Oleg Savelos <olegsavelos@gmail.com>
2025-04-30 13:55:50 +0300

    remaining time message.

---
+++ Server/Options/ClientTimeNotificationOptions.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Server.Options
+{
+    [OptionsConfigurationSection("CLIENTNOTIFICATIONS")]
+    [StoreOptionsGroup("CLIENT_NOTIFICATIONS")]
+    [MessagePack.MessagePackObject()]
+    public sealed class ClientNotificationOptions : IStoreOptions
+    {
+        [Name("Remaining time notification message", "SERVER_OPTION_CLIENT_NOTIFICATION_OPTIONS_NAME")]
+        [ExtendedDescription("Specifies remaining time notification message", "SERVER_OPTION_CLIENT_NOTIFICATION_OPTIONS_DESCRIPTION")]
+        [StoreOptionKey("REMAINING_TIME_NOTIFICATION_MESSAGE")]
+        [MessagePack.Key(0)]
+        public string? RemainingTimeNotificationMessage { get; init; }
+    }
+}
