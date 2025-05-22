d43962e7bcd2480a846fb5b474ff631d9e103bda
klapsis-netprojects <klapsis@netprojects.gr>
2025-05-22 16:51:31 +0300

    Updates.

--- Shared/Enumerations/TaskType.cs
+++ Shared/Enumerations/TaskType.cs
@@ -1,4 +1,6 @@
-﻿namespace Gizmo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo
 {
     /// <summary>
     /// Task types.
@@ -9,24 +11,28 @@
         /// Process.
         /// </summary>
         [Localized("TASK_PROCESS")]
+        [Name("Process", "TASK_TYPE_PROCESS")]
         Process = 0,
 
         /// <summary>
         /// Script.
         /// </summary>
         [Localized("TASK_SCRIPT")]
+        [Name("Script", "TASK_TYPE_SCRIPT")]
         Script = 1,
 
         /// <summary>
         /// Notification.
         /// </summary>
         [Localized("TASK_NOTIFICATION")]
+        [Name("Notification", "TASK_TYPE_NOTIFICATION")]
         Notification = 4,
 
         /// <summary>
         /// Junction.
         /// </summary>
         [Localized("TASK_JUNCTION")]
+        [Name("Junction", "TASK_TYPE_JUNCTION")]
         Junction = 5
     }
 }
