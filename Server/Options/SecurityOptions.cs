8e2c3f7866efd64c5a6cd8fb10401ad401712be7
Oleg Savelos <olegsavelos@gmail.com>
2025-05-23 17:25:13 +0300

    database based secret key.

---
+++ Server/Options/SecurityOptions.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Server.Options
+{
+    [OptionsConfigurationSection("SECURITY")]
+    [StoreOptionsGroup("SECURITY")]
+    [MessagePack.MessagePackObject()]
+    public sealed class SecurityOptions : IStoreOptions
+    {
+        [Name("JWT secret key")]
+        [ExtendedDescription("Specifies jwt secret key")]
+        [StoreOptionKey("SECRET_KEY")]
+        [MessagePack.Key(0)]
+        public string? SecretKey { get; init; }
+    }
+}
