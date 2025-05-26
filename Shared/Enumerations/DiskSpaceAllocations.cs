fc4b51e6cca773a4448fb1839f8301e427e6550b
klapsis-netprojects <klapsis@netprojects.gr>
2025-05-26 14:53:36 +0300

    Updates.

--- Shared/Enumerations/DiskSpaceAllocations.cs
+++ Shared/Enumerations/DiskSpaceAllocations.cs
@@ -1,34 +1,49 @@
-﻿namespace Gizmo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo
 { 
     public enum DiskSpaceAllocations
     {
         /// <summary>
         /// Zero.
         /// </summary>
+        [Name("0%", "DISK_SPACE_ALLOCATIONS_ZERO")]
         Zero = 0,
+
         /// <summary>
         /// Five percent.
         /// </summary>
+        [Name("5%", "DISK_SPACE_ALLOCATIONS_FIVE")]
         Five = 5,
+
         /// <summary>
         /// Ten percent.
         /// </summary>
+        [Name("10%", "DISK_SPACE_ALLOCATIONS_TEN")]
         Ten = 10,
+
         /// <summary>
         /// Fifteen percent.
         /// </summary>
+        [Name("15%", "DISK_SPACE_ALLOCATIONS_FIFTEEN")]
         FifTeen = 15,
+
         /// <summary>
         /// Twenty percent.
         /// </summary>
+        [Name("20%", "DISK_SPACE_ALLOCATIONS_TWENTY")]
         Twenty = 20,
+
         /// <summary>
         /// Twenty five percent.
         /// </summary>
+        [Name("25%", "DISK_SPACE_ALLOCATIONS_TWENTY_FIVE")]
         TwentyFive = 25,
+
         /// <summary>
         /// Thirty percent.
         /// </summary>
-        Thirty = 30,
+        [Name("30%", "DISK_SPACE_ALLOCATIONS_THIRTY")]
+        Thirty = 30
     }
 }
