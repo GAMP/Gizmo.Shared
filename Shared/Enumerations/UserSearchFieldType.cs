1fbeff8ad597378bbc43ba06371587d01037a01e
Oleg Savelos <olegsavelos@gmail.com>
2025-05-30 12:27:12 +0300

    search fields.

---
+++ Shared/Enumerations/UserSearchFieldType.cs
@@ -0,0 +1,17 @@
+﻿namespace Gizmo
+{
+    /// <summary>
+    /// User search field types.
+    /// </summary>
+    public enum UserSearchFieldType
+    {
+        MobilePhone = 0,
+        Username = 1,
+        Email = 2,   
+        FirstName = 3,
+        LastName = 4,
+        Attributes = 5,       
+        Phone = 6,
+        UserGroup = 7,
+    }
+}
