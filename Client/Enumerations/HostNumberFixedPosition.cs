429dd7c1a853f8f3aa078c99150e02f2d23cb305
klapsis-netprojects <klapsis@netprojects.gr>
2025-11-11 11:21:38 +0200

    Updates.

--- Client/Enumerations/HostNumberFixedPosition.cs
+++ Client/Enumerations/HostNumberFixedPosition.cs
@@ -9,30 +9,39 @@ namespace Gizmo.Shared.Client.Enumerations
     /// </summary>
     public enum HostNumberFixedPosition
     {
+        [Name("Top Right", "HOST_NUMBER_FIXED_POSITION_TOP_RIGHT")]
         [Display(Name = "top-right")]
         TopRight = 0,
-    
+
+        [Name("Top Center", "HOST_NUMBER_FIXED_POSITION_TOP_CENTER")]
         [Display(Name = "top-center")]
         TopCenter = 1,
-    
+
+        [Name("Top Left", "HOST_NUMBER_FIXED_POSITION_TOP_LEFT")]
         [Display(Name = "top-left")]
         TopLeft = 2,
-    
+
+        [Name("Center Right", "HOST_NUMBER_FIXED_POSITION_CENTER_RIGHT")]
         [Display(Name = "center-right")]
         CenterRight = 3,
-    
+
+        [Name("Center Screen", "HOST_NUMBER_FIXED_POSITION_CENTER_SCREEN")]
         [Display(Name = "center-screen")]
         CenterScreen = 4,
-    
+
+        [Name("Center Left", "HOST_NUMBER_FIXED_POSITION_CENTER_LEFT")]
         [Display(Name = "center-left")]
         CenterLeft = 5,
-    
+
+        [Name("Bottom Right", "HOST_NUMBER_FIXED_POSITION_BOTTOM_RIGHT")]
         [Display(Name = "bottom-right")]
         BottomRight = 6,
-    
+
+        [Name("Bottom Center", "HOST_NUMBER_FIXED_POSITION_BOTTOM_CENTER")]
         [Display(Name = "bottom-center")]
         BottomCenter = 7,
-    
+
+        [Name("Bottom Left", "HOST_NUMBER_FIXED_POSITION_BOTTOM_LEFT")]
         [Display(Name = "bottom-left")]
         BottomLeft = 8
     }
--- Client/Enumerations/HostNumberPositioning.cs
+++ Client/Enumerations/HostNumberPositioning.cs
@@ -1,19 +1,40 @@
-﻿namespace Gizmo.Client.UI.View.Constants
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.Client.UI.View.Constants
 {
     /// <summary>
     /// Host number movement models
     /// </summary>
     public enum HostNumberPositioning
     {
+        [Name("Clockwise", "HOST_NUMBER_POSITIONING_CLOCKWISE")]
         Clockwise = 0,
+
+        [Name("Counterclockwise", "HOST_NUMBER_POSITIONING_COUNTERCLOCKWISE")]
         Counterclockwise = 1,
+
+        [Name("Horizontal Top", "HOST_NUMBER_POSITIONING_HORIZONTAL_TOP")]
         HorizontalTop = 2,
+
+        [Name("Horizontal Center", "HOST_NUMBER_POSITIONING_HORIZONTAL_CENTER")]
         HorizontalCenter = 3,
+
+        [Name("Horizontal Bottom", "HOST_NUMBER_POSITIONING_HORIZONTAL_BOTTOM")]
         HorizontalBottom = 4,
+
+        [Name("Vertical Left", "HOST_NUMBER_POSITIONING_VERTICAL_LEFT")]
         VerticalLeft = 5,
+
+        [Name("Vertical Center", "HOST_NUMBER_POSITIONING_VERTICAL_CENTER")]
         VerticalCenter = 6,
+
+        [Name("Vertical Right", "HOST_NUMBER_POSITIONING_VERTICAL_RIGHT")]
         VerticalRight = 7,
+
+        [Name("Wall Bounce", "HOST_NUMBER_POSITIONING_WALL_BOUNCE")]
         WallBounce = 8,
+
+        [Name("Fixed", "HOST_NUMBER_POSITIONING_FIXED")]
         Fixed = 9
     }
 }
