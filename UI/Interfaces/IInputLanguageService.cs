22107d999b0200f4013543a4e7eb959f180fba1b
Oleg Savelos <olegsavelos@gmail.com>
2026-01-11 12:17:54 +0200

    add doc.

--- UI/Interfaces/IInputLanguageService.cs
+++ UI/Interfaces/IInputLanguageService.cs
@@ -19,10 +19,23 @@ namespace Gizmo.UI
         /// </summary>
         IEnumerable<CultureInfo> AvailableInputLanguages { get; }
 
+        /// <summary>
+        /// Gets <see cref="CultureInfo"/> based on two letter iso name.
+        /// </summary>
+        /// <param name="twoLetterISOLanguageName"></param>
+        /// <returns></returns>
         CultureInfo? GetLanguage(string twoLetterISOLanguageName);
-        
+
+        /// <summary>
+        /// Sets current input language to desired culture.
+        /// </summary>
+        /// <param name="currentInputLanguage">Language culture.</param>
+        /// <returns></returns>
         Task SetCurrentInputLanguageAsync(CultureInfo currentInputLanguage);
-        
+
+        /// <summary>
+        /// Gets current input language.
+        /// </summary>
         CultureInfo CurrentInputLanguage { get; }
     }
 }
