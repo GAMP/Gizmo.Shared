2451ac299d7323dd6c354d4da75cab9cfdcaeda8
Oleg Savelos <olegsavelos@gmail.com>
2026-01-25 16:26:51 +0200

    remove unused attribute.

--- Shared/Enumerations/VatRates.cs
+++ Shared/Enumerations/VatRates.cs
@@ -10,7 +10,6 @@ namespace Gizmo
         /// <summary>
         /// None (none)
         /// </summary>
-        [Localized("VAT_RATES_RU_NONE")]
         [TaxSystemCountry(TaxSystemCountry.Russia)]
         [Name("None", "VAT_RATES_RUSSIA_NONE")]
         RU_none = 0,
@@ -18,7 +17,6 @@ namespace Gizmo
         /// <summary>
         /// Zero VAT (vat0)
         /// </summary>
-        [Localized("VAT_RATES_RU_VAT0")]
         [TaxSystemCountry(TaxSystemCountry.Russia)]
         [Name("VAT0", "VAT_RATES_RUSSIA_VAT0")]
         RU_vat0 = 1,
@@ -26,7 +24,6 @@ namespace Gizmo
         /// <summary>
         /// 10% VAT (vat10)
         /// </summary>
-        [Localized("VAT_RATES_RU_VAT10")]
         [TaxSystemCountry(TaxSystemCountry.Russia)]
         [Name("VAT10", "VAT_RATES_RUSSIA_VAT10")]
         RU_vat10 = 2,
@@ -34,7 +31,6 @@ namespace Gizmo
         /// <summary>   
         /// 20% VAT (vat20)
         /// </summary>
-        [Localized("VAT_RATES_RU_VAT20")]
         [TaxSystemCountry(TaxSystemCountry.Russia)]
         [Name("VAT20", "VAT_RATES_RUSSIA_VAT20")]
         RU_vat20 = 3,
