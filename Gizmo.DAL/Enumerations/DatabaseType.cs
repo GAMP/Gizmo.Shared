501852e05b79f3599c682671a564865206ec8886
Oleg Savelos <olegsavelos@gmail.com>
2026-01-27 09:34:18 +0200

    updates.

---
+++ Gizmo.DAL/Enumerations/DatabaseType.cs
@@ -0,0 +1,38 @@
+﻿namespace Gizmo.DAL
+{
+    /// <summary>
+    /// Database types.
+    /// </summary>
+    public enum DatabaseType
+    {
+        /// <summary>
+        /// MYSQL.
+        /// </summary>
+        MYSQL = 0,
+
+        /// <summary>
+        /// MSSQL EXPRESS.
+        /// </summary>
+        MSSQLEXPRESS = 2,
+
+        /// <summary>
+        /// MS SQL.
+        /// </summary>
+        MSSQL = 1,
+
+        /// <summary>
+        /// LOCAL DB.
+        /// </summary>
+        LOCALDB = 3,
+
+        /// <summary>
+        /// SQL LITE.
+        /// </summary>
+        SQLITE = 4,
+
+        /// <summary>
+        /// POSTGRE.
+        /// </summary>
+        POSTGRE = 5,
+    }
+}
---
+++ Gizmo.DAL/Enumerations/SQLServerAuthentication.cs
@@ -0,0 +1,23 @@
+﻿namespace Gizmo.DAL
+{
+    /// <summary>
+    /// SQL server authentication types.
+    /// </summary>
+    public enum SQLServerAuthentication
+    {
+        /// <summary>
+        /// Unspecified.
+        /// </summary>
+        Unspecified = 0,
+
+        /// <summary>
+        /// Server.
+        /// </summary>
+        Server = 1,
+
+        /// <summary>
+        /// Integrated.
+        /// </summary>
+        Integrated = 2,
+    }
+}
