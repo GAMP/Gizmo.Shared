defbc1ac8e64b905cca63eb49852f3275b5d40a6
Oleg Savelos <olegsavelos@gmail.com>
2026-01-27 10:47:17 +0200

    updates.

---
+++ Gizmo.Server/Security/IGizmoServerPrincipal.cs
@@ -0,0 +1,17 @@
+﻿namespace Gizmo.Server.Security
+{
+    public interface IGizmoServerPrincipal
+    {
+        /// <summary>
+        /// Gets current user role.
+        /// </summary>
+        /// <remarks>Will return <see cref="Gizmo.Server.UserRoles.None"/> when <see cref="UserId"/> is <see langword="null"/>.</remarks>
+        public Gizmo.Server.UserRoles Role { get; }
+
+        /// <summary>
+        /// Gets current user id.
+        /// </summary>
+        /// <remarks>Will be equal to <see langword="null"/> if no user identity attached to calling thread.</remarks>
+        public int? UserId { get; }
+    }
+}
