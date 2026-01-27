47aab5af7ae425924ef7fc8a2e01653d186dce24
Oleg Savelos <olegsavelos@gmail.com>
2026-01-27 10:22:22 +0200

    updates.

---
+++ Gizmo.Web/HttpContextIdentityProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+
+namespace Gizmo.Web
+{
+    /// <summary>
+    /// HTTP Context identity provider.
+    /// </summary>
+    public abstract class HttpContextIdentityProvider
+    {        
+        /// <summary>
+        /// Gets current instance.
+        /// </summary>
+        public static HttpContextIdentityProvider? Current
+        {
+            get; 
+            set;
+        }
+
+        /// <summary>
+        /// Gets current principal.
+        /// </summary>
+        public virtual ClaimsPrincipal? CurrentPrincipal
+        {
+            get;
+        } 
+    }
+}
