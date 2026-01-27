abc8b00d1845b1962fb9a893e7518d41af82b613
Oleg Savelos <olegsavelos@gmail.com>
2026-01-27 08:45:36 +0200

    updates.

---
+++ Gizmo.DAL/Enumerations/EntityEventType.cs
@@ -0,0 +1,21 @@
+﻿namespace Gizmo.DAL
+{
+    /// <summary>
+    /// Entity event type.
+    /// </summary>
+    public enum EntityEventType
+    {
+        /// <summary>
+        /// Added.
+        /// </summary>
+        Added,
+        /// <summary>
+        /// Removed.
+        /// </summary>
+        Removed,
+        /// <summary>
+        /// Modified.
+        /// </summary>
+        Modified,
+    }
+}
---
+++ Gizmo.DAL/Interfaces/IGizmoDbContextProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace Gizmo.DAL
+{
+    /// <summary>
+    /// Database context provider interface.
+    /// </summary>
+    public interface IGizmoDbContextProvider //TODO: IDisposable?
+    {
+        #region FUNCTIONS
+
+        /// <summary>
+        /// Gets database context.
+        /// </summary>
+        /// <returns>New context instance.</returns>
+        IGizmoDBContext GetDbContext();
+
+        /// <summary>
+        /// Gets non-proxy database context.
+        /// </summary>
+        /// <returns>New context instance.</returns>
+        IGizmoDBContext GetDbNonProxyContext();
+
+        #endregion
+    }
+}
