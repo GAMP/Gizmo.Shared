d75747f6ed884e062a0907c7ae379851bd966924
Andrei Pestunov <masterlifting@gmail.com>
2026-03-09 20:16:25 +0100

    add RemoteControlHotkeysOptions class for hotkey configuration

---
+++ Gizmo.Server/Options/RemoteControlHotkeysOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Server.Options
+{
+    [OptionsConfigurationSection("RemoteControl:Hotkeys")]
+    [StoreOptionsGroup("REMOTE_CONTROL_HOTKEYS")]
+    [MessagePack.MessagePackObject]
+    public sealed class RemoteControlHotkeysOptions : IStoreOptions
+    {
+        [Name("Viewer hotkey definitions")]
+        [ExtendedDescription("JSON object of hotkey-to-label pairs.")]
+        [StoreOptionKey("DEFINITIONS")]
+        [MessagePack.Key(0)]
+        public string? Definitions { get; init; }
+    }
+}
